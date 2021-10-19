ea173307828ae15191106e21f5182d0b5aef4d20
saakibislam <islam.sakibul@gmail.com>
2021-10-19 23:32:41 +0600

    fontawesome removed

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,5 +1,3 @@
-import { faCheck, faCheckCircle, faCircleNotch } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
 const Services = () => {
